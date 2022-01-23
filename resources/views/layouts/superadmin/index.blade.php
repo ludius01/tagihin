600d7a7660cef94b97080c404e7500271fe6003b
ludius01 <ludi.arjan1@gmail.com>
2022-01-23 21:19:09 +0700

    update

--- resources/views/layouts/superadmin/index.blade.php
+++ resources/views/layouts/superadmin/index.blade.php
@@ -42,7 +42,7 @@
 					<!--begin::Logo-->
 					<div class="aside-logo d-none d-lg-flex flex-column align-items-center flex-column-auto py-8" id="kt_aside_logo">
 						<a href="">
-							<img alt="Logo" src="{{ asset("assets/media/logos/logo-kemnaker-white.png")}}" class="h-55px" />
+							<img alt="Logo" src="{{ asset("assets/media/logoputih1.png")}}" class="h-55px" />
 						</a>
 					</div>
 					<!--end::Logo-->
@@ -139,7 +139,7 @@
 							<!--begin::Mobile logo-->
 							<div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
 								<a href="#" class="d-lg-none">
-									<img alt="Logo" src="{{ asset("assets/media/logos/favicon.ico")}}" class="h-30px" />
+									<img alt="Logo" src="{{ asset("assets/media/logobiru1.png")}}" class="h-30px" />
 								</a>
 							</div>
 							<!--end::Mobile logo-->
@@ -174,7 +174,7 @@
 										<div class="d-flex align-items-center ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
 											<!--begin::Menu wrapper-->
 											<div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-												<img src="{{ asset("assets/media/logos/favicon.ico")}}" alt="user" />
+												<img src="{{ asset("assets/media/logobiru1.png")}}" alt="user" />
 											</div>
 											<!--begin::Menu-->
 											<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
@@ -183,7 +183,7 @@
 													<div class="menu-content d-flex align-items-center px-3">
 														<!--begin::Avatar-->
 														<div class="symbol symbol-50px me-5">
-															<img alt="Logo" src="{{ asset("assets/media/logos/favicon.ico")}}" />
+															<img alt="Logo" src="{{ asset("assets/media/logobiru1.png")}}" />
 														</div>
 														<!--end::Avatar-->
 														<!--begin::Username-->
